efbd09e5c0be67c3557ab4d9dca76bcef78e85d2
ThanhNhan2101 <nhannguyen210110@gmail.com>
2026-04-20 17:20:38 +0700

    fix file static

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -88,7 +88,11 @@ TEMPLATES = [
 # STATIC & MEDIA (QUAN TRỌNG)
 # ─────────────────────────────
 STATIC_URL = '/static/' 
-STATIC_ROOT = '/app/staticfiles'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
